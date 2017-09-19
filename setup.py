b5b846f1416f73aedbd32fd40345c7ec5aa9fd02
Erik Henning Thiede <thiede@uchicago.edu>
2017-09-19 16:52:16 -0500

    Added numpy, scipy as dependencies

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'numpy', 'scipy'
     ],
     extras_require={
         # eg:
