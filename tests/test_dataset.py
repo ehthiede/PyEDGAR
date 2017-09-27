0b90964b010c8990deeea2eb14444d521f8ce9ce
Erik Henning Thiede <ehthiede@gmail.com>
2017-09-26 20:08:22 -0500

    Reverting to state of project before failed merge attempt

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -15,8 +15,8 @@ import pytest
 
 from pyedgar import DynamicalDataset
 
-lags = [None] + range(1, 4)
-timesteps = [None, 1., 0.5, 2, 1E4]
+lags = range(1, 4)
+timesteps = [1., 0.5, 2, 1E4]
 
 
 class TestDatasetCreation(object):
@@ -24,32 +24,39 @@ class TestDatasetCreation(object):
     @pytest.mark.parametrize('timestep', timesteps)
     def test_from_flat(self, working_flat_and_tlist, lag, timestep):
         flat, traj_edges, tlist = working_flat_and_tlist
-        DS_from_flat = DynamicalDataset((flat, traj_edges), lag=lag, timestep=timestep)
-        assert(np.all(DS_from_flat.traj_edges == traj_edges))
-        assert(np.all(DS_from_flat.flat_traj == flat))
-        if lag is None:
-            assert(np.all(DS_from_flat.lag == 1))
-        else:
-            assert(np.all(DS_from_flat.lag == lag))
-        if timestep is None:
-            assert(np.all(DS_from_flat.timestep == 1.))
-        else:
-            assert(np.all(DS_from_flat.timestep == timestep))
+        ds_from_flat = DynamicalDataset((flat, traj_edges), lag=lag, timestep=timestep)
+        assert(np.all(ds_from_flat.traj_edges == traj_edges))
+        assert(np.all(ds_from_flat.flat_traj == flat))
+        assert(ds_from_flat.lag == lag)
+        assert(ds_from_flat.timestep == timestep)
 
-    def test_from_tlist(self, working_flat_and_tlist):
+    @pytest.mark.parametrize('lag', lags)
+    @pytest.mark.parametrize('timestep', timesteps)
+    def test_from_tlist(self, working_flat_and_tlist, lag, timestep):
         flat, traj_edges, tlist = working_flat_and_tlist
-        DS_from_flat = DynamicalDataset(tlist)
-        assert(np.all(DS_from_flat.traj_edges == traj_edges))
-        assert(np.all(DS_from_flat.flat_traj == flat))
-        assert(np.all(DS_from_flat.lag == 1))
-        assert(np.all(DS_from_flat.timestep == 1.))
+        ds_from_flat = DynamicalDataset(tlist, lag=lag, timestep=timestep)
+        assert(np.all(ds_from_flat.traj_edges == traj_edges))
+        assert(np.all(ds_from_flat.flat_traj == flat))
+        assert(np.all(ds_from_flat.lag == lag))
+        assert(np.all(ds_from_flat.timestep == timestep))
 
-    def test_from_single_traj(self, working_flat_and_tlist):
+    @pytest.mark.parametrize('lag', lags)
+    @pytest.mark.parametrize('timestep', timesteps)
+    def test_from_single_traj(self, working_flat_and_tlist, lag, timestep):
         flat, traj_edges, tlist = working_flat_and_tlist
         traj = tlist[0]
         traj_edges = np.array([0, len(traj)])
-        DS_from_flat = DynamicalDataset(traj)
-        assert(np.all(DS_from_flat.traj_edges == traj_edges))
-        assert(np.all(DS_from_flat.flat_traj == traj))
-        assert(np.all(DS_from_flat.lag == 1))
-        assert(np.all(DS_from_flat.timestep == 1.))
+        ds_from_flat = DynamicalDataset(traj, lag=lag, timestep=timestep)
+        assert(np.all(ds_from_flat.traj_edges == traj_edges))
+        assert(np.all(ds_from_flat.flat_traj == traj))
+        assert(np.all(ds_from_flat.lag == lag))
+        assert(np.all(ds_from_flat.timestep == timestep))
+
+    def test_bad_input(self):
+        with pytest.raises(ValueError) as excinfo:
+            ds_bad = DynamicalDataset('bad_input')
+
+
+
+class TestDataReturn():
+    
